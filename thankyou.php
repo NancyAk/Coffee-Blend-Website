e6a1b187c9388dbf9beaa115d3c4af3665cc6535
NancyAk <146520443+NancyAk@users.noreply.github.com>
2024-01-10 17:15:27 +0200

    Create thankyou.php

---
+++ thankyou.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exciting News: Your Order is En Route!</title>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            text-align: center;
+            background-color: #f6f6f6;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+
+        .thank-you-container {
+            max-width: 600px;
+            width: 100%;
+            padding: 30px;
+            background-color: #ffffff;
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+            border-radius: 10px;
+            text-align: center;
+        }
+
+        .thank-you-image {
+            max-width: 50%;
+            height: auto;
+            margin-bottom: 30px;
+            border-radius: 10px;
+        }
+
+        .thank-you-message {
+            font-size: 24px;
+            color: #333;
+            margin-bottom: 20px;
+        }
+
+        .back-to-home {
+            display: inline-block;
+            padding: 12px 24px;
+            background-color: #fac031;
+            color: #333;
+            text-decoration: none;
+            font-weight: bold;
+            border-radius: 5px;
+            transition: background-color 0.3s;
+        }
+
+        .back-to-home:hover {
+            background-color: #333;
+            color: #fff;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="thank-you-container">
+        <img src="image/thanks.png" alt="Thank You Image" class="thank-you-image">
+        <div class="thank-you-message">
+            <p>Your taste adventure is on the way! Get ready for a journey of flavor.</p>
+            <!-- You can customize this message with additional details -->
+        </div>
+        <a href="index.html" class="back-to-home">Back to Home</a>
+    </div>
+</body>
+
+</html>
